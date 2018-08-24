962e2753351e1bfe9014cf97e0ce695c3c190238
Cristianx10 <Cristianx10@users.noreply.github.com>
2018-08-24 14:52:17 -0500

    Se inicio la parte de programacion

--- app/src/main/java/com/example/ecosistemas/quiz1/MainActivity.java
+++ app/src/main/java/com/example/ecosistemas/quiz1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ecosistemas.quiz1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,6 +23,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv_titulo= findViewById(R.id.tv_titulo);
+        tv_confirmacion = findViewById(R.id.tv_confirmar);
+        et_name= findViewById(R.id.et_name);
+        et_password= findViewById(R.id.et_password);
+        btn_ingresar= findViewById(R.id.btn_ingresar);
+        btn_cerrar= findViewById(R.id.btn_cerrar);
+
+        btn_ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent solicitud = new Intent();
+
+                String user = et_name.getText().toString();
+                String password = et_password.getText().toString();
+
+                //Usuario registrados
+                String dataUser = "Adminsitrador";
+                String dataPassword = "123456";
+
+                if(user.equals(dataUser) && password.equals(dataPassword)){
+
+
+
+                }
+
+            }
+        });
 
     }
 
